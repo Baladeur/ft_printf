df0610aa9bad77283c1c752bf7697814642461ba
Theo FERRIEUX <tferrieu@e3r8p14.42.fr>
2019-04-10 20:09:13 +0200

    Float fixes | updated libft

--- srcs/convert_float.c
+++ srcs/convert_float.c
@@ -6,37 +6,38 @@
 /*   By: tferrieu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 14:28:20 by tferrieu          #+#    #+#             */
-/*   Updated: 2019/04/10 18:37:54 by tferrieu         ###   ########.fr       */
+/*   Updated: 2019/04/10 20:04:43 by tferrieu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static char	*ftoa_custom2(long long int n, int *tab, int p)
+static char	*ftoa_custom2(long double n, int *tab, int p, int j)
 {
 	char	*str;
 	int		k;
 	int		i;
 
-	if (!n)
+	if ((k = p) >= 0 && n == 0.0)
 	{
 		str = ft_strmake('0', tab[1] + 2 - !(tab[1] || tab[4]));
 		if (str[1])
 			str[1] = '.';
+		tab[5] = !(1 / n > 0) ? '-' : tab[5];
 		return (str);
 	}
-	if (!(str = ft_strmake(' ', p + 1 - !(tab[1] || tab[4]))))
+	if (!(str = ft_strmake(' ', p - j + tab[1] + 1 + (tab[1] > j)
+										- !(tab[1] || tab[4]))))
 		return (NULL);
-	k = p;
-	i = 0;
-	while (str[i])
+	i = -1;
+	while (str[++i] && i <= p)
 	{
-		str[i] = (n / ft_pow(10, k - 1)) % 10 + 48;
-		k--;
-		if (k == tab[1] && str[++i])
+		str[i] = (long long int)(n / (long double)ft_pow_u(10, --k)) % 10 + 48;
+		if (k == (j - (tab[1] > j)) && str[++i])
 			str[i] = '.';
-		i++;
 	}
+	while (str[i])
+		str[i++] = '0';
 	return (str);
 }
 
@@ -47,11 +48,11 @@ static char	*ftoa_custom(va_list arglist, int *tab)
 	int			k;
 	int			p;
 
-	if (tab[6] == 'L')
+	if ((p = 1) > 0 && tab[6] == 'L')
 		n = (long double)(va_arg(arglist, long double));
 	else
 		n = (long double)(va_arg(arglist, double));
-	if ((p = 1) > 0 && n < 0. && (tab[5] = '-'))
+	if (n < 0. && (tab[5] = '-'))
 		n *= -1.;
 	if (n >= 1.0 / 0.0 && n <= 1.0 / 0.0)
 		return (ft_strdup("inf"));
@@ -59,14 +60,14 @@ static char	*ftoa_custom(va_list arglist, int *tab)
 		return (ft_strdup("nan"));
 	tab[1] = tab[1] < 0 ? 6 : tab[1];
 	k = 0;
-	while (k < tab[1] && ++k)
+	while (k < tab[1] && ++k && n != (long double)((long long int)n))
 		n *= 10.;
 	if (n - (long double)((long long int)n) > 0.5)
 		n += 1.;
 	tmp = n;
 	while (tmp >= 10. && ++p)
 		tmp /= 10.;
-	return (ftoa_custom2((long long int)n, tab, p));
+	return (ftoa_custom2(n, tab, p, k));
 }
 
 static char	*gather_arg(va_list arglist, int *tab, int *len)
